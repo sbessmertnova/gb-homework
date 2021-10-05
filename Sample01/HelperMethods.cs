c60745196ce28d0ac82af91c96f572b266ba28b9
sbessmertnova <minnij@yandex.ru>
2021-10-05 13:23:09 +0300

    Add project files.

---
+++ Sample01/HelperMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample01
+{
+    class HelperMethods
+    {
+        public static void Print(string output, int left, int top)
+        {
+            Console.SetCursorPosition(left, top);
+            Console.WriteLine(output);
+        }
+
+        public static void Pause(string output)
+        {
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+
+
+    }
+}
